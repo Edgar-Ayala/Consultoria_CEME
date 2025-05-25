ab91f022efb650175bf7c40a891141b3169a7255
Edgar-Ayala <ayalalereaedgarpablo@gmail.com>
2025-05-25 00:18:28 -0300

    INICIO DEL GIT
    Se creo para importar todo

---
+++ CEME/CEME/src/main/java/com/RobKon/CEME/CemeApplication.java
@@ -0,0 +1,13 @@
+package com.RobKon.CEME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemeApplication.class, args);
+	}
+
+}
---
+++ CEME/CEME/src/test/java/com/RobKon/CEME/CemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RobKon.CEME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
